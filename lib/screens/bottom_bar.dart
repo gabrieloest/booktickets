0c5c139b22e19052e9f1cbc2fcc574f5b6d145b6
Gabriel Oest <gabriel.oest@etermax.com.uy>
2022-12-14 02:00:34 -0300

    Add Home screen

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:booktickets/screens/home_screen.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -14,7 +15,7 @@ class _BottomBarState extends State<BottomBar> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
-    const Text("Home"),
+    HomeScreen(),
     const Text("Search"),
     const Text("Tickets"),
     const Text("Profile"),
